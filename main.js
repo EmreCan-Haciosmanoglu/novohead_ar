db87cbe8ee1253e4d260fa190344cf91f064815b
emrecan-haciosmanoglu <emrecan.haciosmanoglu@agu.edu.tr>
2021-11-30 09:45:44 +0300

    Tweaks

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 async function activateXR() {
-    // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
     const canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
     const gl = canvas.getContext("webgl", { xrCompatible: true });
@@ -7,15 +6,6 @@ async function activateXR() {
         alert("Failure!!!")
         return
     }
-    const scene = new THREE.Scene();
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(10, 15, 10);
-    //scene.add(directionalLight);
-    const light = new THREE.AmbientLight(0x404040,4); // soft white light
-    scene.add(light);
-
-    // Set up the WebGLRenderer, which handles rendering to the session's base layer.
     const renderer = new THREE.WebGLRenderer({
         alpha: true,
         preserveDrawingBuffer: true,
@@ -24,32 +14,26 @@ async function activateXR() {
     });
     renderer.autoClear = false;
 
-    // The API directly updates the camera matrices.
-    // Disable matrix auto updates so three.js doesn't attempt
-    // to handle the matrices independently.
+    const session = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'] });
+    session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
+
+    const referenceSpace = await session.requestReferenceSpace('local');
+    const viewerSpace = await session.requestReferenceSpace('viewer');
+    const hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
+
     const camera = new THREE.PerspectiveCamera(45, 9.0 / 16.0);
     camera.matrixAutoUpdate = false;
 
-    // Initialize a WebXR session using "immersive-ar".
-    const session = await navigator.xr.requestSession("immersive-ar", { requiredFeatures: ['hit-test'] });
-    session.updateRenderState({
-        baseLayer: new XRWebGLLayer(session, gl)
-    });
+    const scene = new THREE.Scene();
 
-    // A 'local' reference space has a native origin that is located
-    // near the viewer's position at the time the session was created.
-    const referenceSpace = await session.requestReferenceSpace('local');
-
-    // Create another XRReferenceSpace that has the viewer as the origin.
-    const viewerSpace = await session.requestReferenceSpace('viewer');
-    // Perform hit testing using the viewer as origin.
-    const hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
-
+    const light = new THREE.AmbientLight(0x404040, 4);
+    scene.add(light);
     var main_group = new THREE.Group();
     var clickables = new THREE.Group();
     var unclickables = new THREE.Group();
     main_group.add(clickables);
     main_group.add(unclickables);
+    scene.add(main_group);
     let box, worm, dirt, cross, box_lid_red, box_lid_zebra, box_lid_white, button_to_change_texture;
     let button_to_open_and_close_lid, info_button_1, info_button_2, info_button_3, info_button_4;
     let info_button_5, info_1, info_2, info_3, info_4, info_5, apple;
@@ -57,6 +41,7 @@ async function activateXR() {
     loader.load("models/box/box.gltf", function (gltf) {
         box = gltf.scene;
         unclickables.add(box);
+        console.log(box);
     });
     loader.load("models/box/worm.gltf", function (gltf) {
         worm = gltf.scene;
@@ -143,7 +128,6 @@ async function activateXR() {
         unclickables.add(apple);
     });
 
-    scene.add(main_group);
 
     var textureID = 0;
     function do_job(object) {
@@ -229,15 +213,9 @@ async function activateXR() {
             }
         }
         else if (stage == 1) {
-            // update the picking ray with the camera and mouse position
             raycaster.setFromCamera(mouse, camera);
-
-            // calculate objects intersecting the picking ray
             const intersects = raycaster.intersectObjects(clickables.children, true);
-            //const intersects = raycaster.intersectObject(button_to_change_texture, true);
-
-            if (intersects[0]) do_job(intersects[0].object)
-
+            if (intersects[0]) do_job(intersects[0].object);
         }
     }
 
@@ -245,44 +223,30 @@ async function activateXR() {
 
     const raycaster = new THREE.Raycaster();
     const mouse = new THREE.Vector2();
-    var ori;
 
-    // Create a render loop that allows us to draw on the AR view.
     const onXRFrame = (time, frame) => {
-        // Queue up the next draw request.
         session.requestAnimationFrame(onXRFrame);
-
-        // Bind the graphics framebuffer to the baseLayer's framebuffer
         gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer)
-
-        // Retrieve the pose of the device.
-        // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
         const pose = frame.getViewerPose(referenceSpace);
         if (pose) {
-            // In mobile AR, we only have one view.
             const view = pose.views[0];
-            ori = pose.transform.orientation;
-
             const viewport = session.renderState.baseLayer.getViewport(view);
             renderer.setSize(viewport.width, viewport.height)
-
-            // Use the view's transform matrix and projection matrix to configure the THREE.camera.
             camera.matrix.fromArray(view.transform.matrix)
             camera.projectionMatrix.fromArray(view.projectionMatrix);
             camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert();
             camera.updateMatrixWorld(true);
-
             const hitTestResults = frame.getHitTestResults(hitTestSource);
             if (hitTestResults.length > 0) {
+                // Code here!!!
                 const hitPose = hitTestResults[0].getPose(referenceSpace);
                 if (stage == 0) {
                     main_group.visible = true;
                     main_group.position.set(hitPose.transform.position.x, hitPose.transform.position.y, hitPose.transform.position.z)
                     main_group.updateMatrixWorld(true);
                 }
-            }
 
-            // Render the scene with THREE.WebGLRenderer.
+            }
             renderer.render(scene, camera)
         }
     }
